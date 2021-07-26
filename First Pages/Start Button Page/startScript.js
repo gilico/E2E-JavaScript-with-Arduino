b64470a6c5b4f25a7197fbd2cc35dc81266e9b3e
Gil Cohen <76738594+gilico@users.noreply.github.com>
2021-07-26 20:32:52 +0300

    Add files via upload

---
+++ First Pages/Start Button Page/startScript.js
@@ -0,0 +1,22 @@
+﻿var loadGif = document.getElementById("loading");
+var gif = document.createElement('div');
+var button = document.getElementById('start');
+
+button.addEventListener('click', function () {
+    document.getElementById('audio').play();
+})
+
+function CreateLoadGif() {
+    loadGif.innerHTML = '<div id="gif-div"></div>';
+
+    gif.innerHTML = '<img src="https://media.giphy.com/media/3o7bu3XilJ5BOiSGic/giphy.gif" />';
+
+    loadGif.appendChild(gif);
+    button.target = '_blank';
+
+    setTimeout(() => {
+        window.open('../../trivia/questionFormat.html');
+        gif.innerHTML = '';
+    }, 2500);
+}
+
