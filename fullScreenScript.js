c01188914d619b585fc9352c863956550a387ac5
Gil Cohen <76738594+gilico@users.noreply.github.com>
2021-08-07 16:11:55 +0300

    Add files via upload

---
+++ fullScreenScript.js
@@ -0,0 +1,15 @@
+﻿
+document.addEventListener("keydown", FullScreen)
+
+function FullScreen(k) {
+    if (k.keyCode == 70) {
+        document.documentElement.requestFullscreen().catch(console.log);
+    }
+}
+
+document.addEventListener("fullscreenchange", () => {
+    console.log("full screen changed!");
+});
+
+
+
